7bbaed6654b4d928b56ad227238513b45bd4c0cb
Meszaros Gyorgy Attila <sirex1993@gmail.com>
2018-10-17 21:50:31 +0200

    get total pages from api

--- src/main/java/com/petproject/demo/api/MovieApiResult.java
+++ src/main/java/com/petproject/demo/api/MovieApiResult.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class MovieApiResult {
     private List<Film> results;
+    private String total_pages;
 
     public List<Film> getResults() {
         return Collections.unmodifiableList(results);
@@ -14,5 +15,13 @@ public class MovieApiResult {
 
     public void setResults(List<Film> results) {
         this.results = results;
+    }
+
+    public int getTotal_pages() {
+        return Integer.parseInt(total_pages);
+    }
+
+    public void setTotal_pages(String total_pages) {
+        this.total_pages = total_pages;
     }
 }
